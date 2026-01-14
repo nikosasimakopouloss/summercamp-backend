9ed618bd40feee3316f2cf08d417f18e6e3531ba
nikosasimakopouloss <nikosasimakopouloss@gmail.com>
2026-01-14 04:30:58 +0200

    Role, Auth zod validations

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,15 +1,19 @@
 import {Router} from "express"
 import * as userCtrl from "../controllers/user.controller";
+import { validate } from "../middlewares/validate.middleware";
+import { validateObjectId } from "../middlewares/validateObjectid.middleware"
+import { createUserSchema, updateUserSchema } from "../validators/user.validator";
+
 
 
 const router = Router();
 
 
 router.get("/", userCtrl.list);
-router.get('/:id', userCtrl.getOne);
-router.post("/", userCtrl.create);
-router.put('/:id', userCtrl.update);
-router.delete('/:id', userCtrl.remove);
+router.get('/:id', validateObjectId('id'), userCtrl.getOne);
+router.post("/", validate(createUserSchema), userCtrl.create);
+router.put('/:id', validateObjectId('id') ,userCtrl.update);
+router.delete('/:id', validateObjectId('id'), userCtrl.remove);
 
 
 export default router;
