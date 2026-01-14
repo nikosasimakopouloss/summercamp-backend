33c9fb438b62008186af69e5484b6cde08eb39d0
nikosasimakopouloss <nikosasimakopouloss@gmail.com>
2026-01-14 05:23:41 +0200

    Authenticate Middleware

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,17 +3,18 @@ import * as userCtrl from "../controllers/user.controller";
 import { validate } from "../middlewares/validate.middleware";
 import { validateObjectId } from "../middlewares/validateObjectid.middleware"
 import { createUserSchema, updateUserSchema } from "../validators/user.validator";
+import { authenticate } from "../middlewares/auth.middleware";
 
 
 
 const router = Router();
 
 
-router.get("/", userCtrl.list);
+router.get("/", authenticate, userCtrl.list);
 router.get('/:id', validateObjectId('id'), userCtrl.getOne);
-router.post("/", validate(createUserSchema), userCtrl.create);
-router.put('/:id', validateObjectId('id') ,userCtrl.update);
-router.delete('/:id', validateObjectId('id'), userCtrl.remove);
+router.post("/", authenticate, validate(createUserSchema), userCtrl.create);
+router.put('/:id', authenticate, validateObjectId('id') ,userCtrl.update);
+router.delete('/:id', authenticate, validateObjectId('id'), userCtrl.remove);
 
 
 export default router;
